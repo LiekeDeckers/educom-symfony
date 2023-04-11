421ea35abdf0a74129a8b8d21ac2f19adbd1c6ce
LiekeDeckers <liekedeckers15@gmail.com>
2023-04-11 15:21:00 +0200

    homepage controller

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -3,16 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
+#[Route('/')]
 class HomepageController extends AbstractController
 {
-    #[Route('/homepage', name: 'app_homepage')]
-    public function index(): Response
-    {
-        return $this->render('homepage/index.html.twig', [
-            'controller_name' => 'HomepageController',
-        ]);
+    #[Route('/', name: 'homepage')]
+    #[Template()]
+    public function index() {
+        return ['controller_name' => 'HomepageController'];
+    }
+
+    #[Route('/backhome', name: 'backhome')]
+    public function backhome() {
+        return $this->redirectToRoute('homepage');
     }
 }
